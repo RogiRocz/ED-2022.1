7ec785445305b6544e9031819d444d1d684b0e5c
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2022-05-02 11:54:02 -0300

    Update Queimada.cpp

--- S03 - Recursão/Queimada.cpp
+++ S03 - Recursão/Queimada.cpp
@@ -1,11 +1,46 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <time.h>
+#include <algorithm>
+
+enum Tipo_terreno{
+    ARVORE,
+    VAZIO,
+    QUEIMADO
+};
 
 using namespace std;
 
-vector<int> produzirMapa(int nc){
+vector<char> produzirMapa(int nc){
+    srand(time(NULL));
+    vector<char> coluna(nc);
+    for (auto &elem : coluna)
+    {
+        int numRandomico = rand() % 3;
+        Tipo_terreno tipo = (Tipo_terreno)numRandomico;
+        switch (numRandomico)
+        {
+        case 0:
+            elem = '#';
+            break;
+        case 1:
+            elem = '.';
+            break;
+            case 2:
+            elem = 'o';
+            break;
+        default:
+            break;
+        }
+        // cout << "Tipo: " << (char)tipo << endl;
+        // elem = tipo;
+    }
     
+
+    // random_shuffle(coluna.begin(), coluna.end());
+
+    return coluna;
 }
 
 int main(){
@@ -15,10 +50,23 @@ int main(){
     int nl, nc, l, c;
     ss >> nl >> nc >> l >> c;
     
-    vector<vector<int>> vet;
-    for(int i = 0; i < nl; i++){
-        vet[i].push_back(produzirMapa(nc));
+    
+    vector<vector<char>> vet(nl);
+    for (auto it = vet.begin(); it != vet.end(); it++)
+    {
+        *it = produzirMapa(nc);
     }
+    
+    for (auto row : vet)
+    {
+        for (auto col : row)
+        {
+            cout << col << ' ';
+        }
+        cout << endl;        
+    }
+    
+    
     
     
     return 0;
