65fb2ad564e9025cd7f617f2c3cf5d779880c292
RogiRocz <rogirocz@gmail.com>
2022-05-03 21:59:54 -0300

    add files

---
+++ S03 - Recursão/FugaRecursiva.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ifstream ifs("Entrada.txt", ifstream::in);
+
+    ifs.close();
+    return 0;
+}
