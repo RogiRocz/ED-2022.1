9a8bc5aea1013a5c14a71983c48de81f9a8e2bc6
RogiRocz <rogirocz@gmail.com>
2022-05-13 21:30:01 -0300

    finish atvd

--- S03 - Recursão/Proximidade.cpp
+++ S03 - Recursão/Proximidade.cpp
@@ -7,13 +7,17 @@ void exibir(string dados)
     cout << dados;
 }
 
-bool verificarAnt(string dados, int prox, int pos, int value)
+int verificarAnt(string dados, int prox, int pos, int value)
 {
     cout << "ANT" << endl;
     cout << "Dados: " << dados << " Prox :" << prox << " Pos: " << pos << " Value: " << value << endl;
-    
+
     if (pos < 0 || prox == 0)
     {
+        if (dados[pos] == (value + '0'))
+        {
+            return 0;
+        }
         return 1;
     }
 
@@ -21,25 +25,29 @@ bool verificarAnt(string dados, int prox, int pos, int value)
     {
         return 0;
     }
-    
+
     return 1 * verificarAnt(dados, prox - 1, pos - 1, value);
 }
 
 int verificarPos(string dados, int prox, int pos, int value)
-{   
+{
     cout << "POS" << endl;
     cout << "Dados: " << dados << " Prox :" << prox << " Pos: " << pos << " Value: " << value << endl;
-    
-    if(pos >= dados.size() || prox == 0)
+
+    if (pos >= dados.size() || prox == 0)
     {
+        if (dados[pos] == (value + '0'))
+        {
+            return 0;
+        }
         return 1;
     }
-    
-    if(dados[pos] == (value + '0'))
+
+    if (dados[pos] == (value + '0'))
     {
         return 0;
     }
-    
+
     return 1 * verificarPos(dados, prox - 1, pos + 1, value);
 }
 
@@ -56,18 +64,19 @@ void saida(string dados, int prox)
         }
         return -1;
     };
-    
+
     int pos = posVazia(dados);
 
-    while(pos != -1){
-        for(int i = 0; i <= prox; i++){
-            if(verificarAnt(dados, prox, pos, i) && verificarPos(dados, prox, pos, i))
+    while (pos != -1)
+    {
+        for (int i = 0; i <= prox; i++)
+        {
+            if (verificarAnt(dados, prox, pos, i) && verificarPos(dados, prox, pos, i))
             {
                 dados[pos] = i + '0';
-                
+                break;
             }
         }
-        cout << "------------------------" << endl;
         pos = posVazia(dados);
     }
     exibir(dados);
@@ -80,7 +89,7 @@ int main(int argc, char const *argv[])
 
     fgets(dados, 100, stdin);
     cin >> prox;
-    
+
     if (!(prox > 0 && prox < 100))
     {
         cerr << "Proximidade so aceita valores de 1 ate 99";
