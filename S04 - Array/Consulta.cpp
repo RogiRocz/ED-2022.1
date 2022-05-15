446ec6905c66c228868579b245a63e19f435bf23
RogiRocz <rogirocz@gmail.com>
2022-05-14 22:34:29 -0300

    formatando a entrada

--- S04 - Array/Consulta.cpp
+++ S04 - Array/Consulta.cpp
@@ -6,15 +6,40 @@ using namespace std;
 
 vector<int> matchingStrings(string e, string c);
 
+string exibir(vector<string> entrada)
+{
+    stringstream ss;
+    for (auto elem : entrada)
+    {
+        ss << elem;
+    }
+
+    return ss.str();
+}
+
 int main(int argc, char const *argv[])
 {
-    int sizeE, sizeC;
-    stringstream ss;
-    string e, c;
+    int sizeCon, sizeBus;
+    string strCon, strBus;
+    vector<string> con, bus;
 
-    cin >> sizeC;
-    cin.getline(c, c.max_size());
-    cout << sizeC << c;
+    cin >> sizeCon;
+
+    for (size_t i = 0; i < sizeCon; i++)
+    {
+        cin >> strCon;
+        con.push_back(strCon);
+    }
+
+    cin >> sizeBus;
+
+    for (size_t i = 0; i < sizeBus; i++)
+    {
+        cin >> strBus;
+        bus.push_back(strBus);
+    }
+
+    cout << sizeCon << exibir(con) << sizeBus << exibir(bus);
 
     return 0;
 }
