0126d87e7c8c695468b58f7fb2086ce53286b7b8
RogiRocz <rogirocz@gmail.com>
2022-05-14 22:52:40 -0300

    finish atvd

--- S04 - Array/Consulta.cpp
+++ S04 - Array/Consulta.cpp
@@ -4,31 +4,48 @@
 
 using namespace std;
 
-vector<int> matchingStrings(string e, string c);
-
-string exibir(vector<string> entrada)
+string exibir(vector<int> entrada)
 {
     stringstream ss;
     for (auto elem : entrada)
     {
-        ss << elem;
+        ss << elem << ' ';
     }
 
     return ss.str();
+}
+
+vector<int> matchingStrings(vector<string> c, vector<string> b)
+{
+    vector<int> result;
+    for (auto elemB : b)
+    {
+        pair<string, int> match(elemB, 0);
+        for (auto elemC : c)
+        {
+            if (elemC == elemB)
+            {
+                match.second++;
+            }
+        }
+        result.push_back(match.second);
+    }
+
+    return result;
 }
 
 int main(int argc, char const *argv[])
 {
     int sizeCon, sizeBus;
     string strCon, strBus;
-    vector<string> con, bus;
+    vector<string> consulta, busca;
 
     cin >> sizeCon;
 
     for (size_t i = 0; i < sizeCon; i++)
     {
         cin >> strCon;
-        con.push_back(strCon);
+        consulta.push_back(strCon);
     }
 
     cin >> sizeBus;
@@ -36,10 +53,11 @@ int main(int argc, char const *argv[])
     for (size_t i = 0; i < sizeBus; i++)
     {
         cin >> strBus;
-        bus.push_back(strBus);
+        busca.push_back(strBus);
     }
 
-    cout << sizeCon << exibir(con) << sizeBus << exibir(bus);
+    cout << exibir(matchingStrings(consulta, busca)) << endl;
+    // cout << sizeCon << " " << exibir(con) << " " << sizeBus << " " << exibir(bus);
 
     return 0;
 }
