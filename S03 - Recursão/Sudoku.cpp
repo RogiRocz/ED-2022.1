b0895540d9b13d79b573ed954f1affdebb7802bd
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2022-05-18 10:30:09 -0300

    Update Sudoku.cpp
    Codigo funciona pro primeiro teste

--- S03 - Recursão/Sudoku.cpp
+++ S03 - Recursão/Sudoku.cpp
@@ -4,13 +4,15 @@
 
 using namespace std;
 
+typedef vector<vector<int>> matInt;
+
 struct Pos{
     int l, c;
     
     Pos(int l, int c) : l(l), c(c){}
 };
 
-void exibir(vector<vector<int>> vet){
+void exibir(matInt vet){
     for(auto row : vet){
         for(auto col : row){
             cout << col << ' ';
@@ -35,13 +37,112 @@ void entrada(vector<vector<int>>& vet, int n){
     }
 }
 
-void resolver(vector<vector<int>> vet){
-    auto posVazia = [](vector<vector<int>>){
-        for(int i = 0){
-            if(elem == 0)
-                return 
+bool verificarAnt(matInt vet, Pos pos, int value, char direcao){
+    switch(direcao){
+        case 'l':
+        // A linha permanece a mesma a coluna varia
+            if(pos.c < 0){
+                return true;
+            }
+            
+            if(vet[pos.l][pos.c] == value){
+                return false;
+            }
+            
+            return verificarAnt(vet, Pos(pos.l, pos.c - 1), value, direcao);
+            break;
+        case 'c':
+            // A coluna permanece a mesma a linha varia
+            if(pos.l < 0){
+                return true;
+            }
+            
+            if(vet[pos.l][pos.c] == value){
+                return false;
+            }
+            
+            return verificarAnt(vet, Pos(pos.l - 1, pos.c), value, direcao);
+            break;
+        default:
+            cout << "Opcao invalida";
+            break;
+    }
+    
+}
+
+bool verificarPos(matInt vet, Pos pos, int value, char direcao){
+    switch(direcao){
+        case 'l':
+        // A linha permanece a mesma a coluna varia
+            if(pos.c == vet.size()){
+                return true;
+            }
+            
+            if(vet[pos.l][pos.c] == value){
+                return false;
+            }
+            
+            return verificarPos(vet, Pos(pos.l, pos.c + 1), value, direcao);
+            break;
+        case 'c':
+            // A coluna permanece a mesma a linha varia
+            if(pos.l == vet.size()){
+                return true;
+            }
+            
+            if(vet[pos.l][pos.c] == value){
+                return false;
+            }
+            
+            return verificarPos(vet, Pos(pos.l + 1, pos.c), value, direcao);
+            break;
+        default:
+            cout << "Opcao invalida";
+            break;
+    }
+}
+
+bool verificarLinha(matInt vet, Pos pos, int value){
+    return verificarAnt(vet, pos, value, 'c') &&
+    verificarPos(vet, pos, value, 'c');
+}
+
+bool verificarColuna(matInt vet, Pos pos, int value){
+    return verificarAnt(vet, pos, value, 'l') &&
+    verificarPos(vet, pos, value, 'l');
+}
+
+int verificaQuadrante(matInt vet);
+
+bool posValida(Pos p){
+    return p.l == -1 || p.c == -1 ? false : true;
+}
+
+void resolver(matInt& vet){
+    auto posVazia = [](vector<vector<int>> vet){
+        for(int i = 0; i < (int)vet.size(); i++){
+            for(int j = 0; j < (int)vet[0].size(); j++){
+                if(vet[i][j] == 0){
+                    return Pos(i, j);
+                }
+            }
         }
+        
+        return Pos(-1, -1);
     };
+    
+    auto pos = posVazia(vet);
+    int l = pos.l, c = pos.c;
+    
+    if(posValida(pos)){
+        for(int i = 1; i <= vet.size(); i++){
+            if(verificarLinha(vet, pos, i) && 
+            verificarColuna(vet, pos, i)){
+                vet[l][c] = i;
+            }
+        }
+        resolver(vet);
+    }
 }
 
 int main() {
