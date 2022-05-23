78258d8533bb36942957fb9bfcd51ecdf10a53e8
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2022-05-23 16:13:47 +0000

    Fiz a resolucao com vector

--- S04 - Array/Fila.cpp
+++ S04 - Array/Fila.cpp
@@ -1,9 +1,52 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
+void exibir(vector<int> vet){
+	cout << "[ ";
+	for(auto elem: vet){
+		cout << elem << ' ';
+	}
+	cout << "]" << endl;
+}
+
+vector<int> resolver(vector<int> inicial, vector<int> sairam){
+	for(auto itS = sairam.begin(); itS != sairam.end(); itS++){
+		for(auto itI = inicial.begin(); itI != inicial.end(); itI++){
+			if(*itS == *itI){
+				inicial.erase(itI);
+				break;
+			}
+		}
+	}
+	return inicial;
+}
+
 int main() {
     int n, m;
-    vector<int> filaInicial, saida;
+    vector<int> filaInicial, sairam;
+	string entrada;
+	
+	cin >> n;
+	
+	for(int i = 0; i < n; i++){
+		int num;
+		cin >> num;
+		filaInicial.push_back(num);
+	}
+	
+	cin >> m;
 
+	for(int i = 0; i < m; i++){
+		int num;
+		cin >> num;
+		sairam.push_back(num);
+	}
+
+	// exibir(filaInicial);
+	// exibir(sairam);
+	exibir(resolver(filaInicial, sairam));
+	
     return 0;
 }
