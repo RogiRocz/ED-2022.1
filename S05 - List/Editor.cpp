cf2c5716e05df6185cc7cad639c0cdb8d22abbf4
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2022-06-20 13:00:33 +0000

    update Editor.cpp

--- S05 - List/Editor.cpp
+++ S05 - List/Editor.cpp
@@ -3,89 +3,87 @@
 
 using namespace std;
 
-void cadaCaracter(string line){
-	for(int i = 0; i <= (int)line.size(); i++){
+void cadaCaracter(string line) {
+	for (int i = 0; i <= (int)line.size(); i++) {
 		cout << "'" << line[i] << "'" << endl;
 	}
 }
 
-bool caracteresAceitos(char c){
-	if((c >= 'a' && c <= 'z') || c == '-'){
+bool caracteresAceitos(char c) {
+	if ((c >= 'a' && c <= 'z') || c == '-') {
 		return true;
 	}
 	return false;
 }
 
-
-string formatar(string line){
+string formatar(string line) {
 	string saida;
 	auto cursor = saida.begin();
 
-	auto mover_cursor_esquerda = [](string::iterator cursor){
-		return *(cursor - 1) != 0;
+	auto mover_cursor_esquerda = [](string saida, string::iterator cursor) {
+		return (cursor - 1) != begin(saida);
 	};
-	
-	auto mover_cursor_direita = [](string::iterator cursor){
-		return *(cursor + 1) != 0;
+
+	auto mover_cursor_direita = [](string saida, string::iterator cursor) {
+		return (cursor + 1) != end(saida);
 	};
-	
-	for(int i = 0; i <= (int)line.size(); i++){
+
+	for (int i = 0; i <= (int)line.size(); i++) {
 		auto letter = line[i];
-		if(caracteresAceitos(letter)){
+		if (caracteresAceitos(letter)) {
 			saida += letter;
 		}
-		if(letter == 'R'){
+		if (letter == 'R') {
 			letter = '\n';
 			saida += letter;
 		}
-		if(letter == 'B'){
-			if(mover_cursor_esquerda(cursor)){
+		if (letter == 'B') {
+			if (mover_cursor_esquerda(saida, cursor)) {
 				cursor--;
-				cursor = line.erase(cursor);
-				cursor = line.erase(cursor);
-			}			
-			saida += letter;
-		}
-		if(letter == 'D'){
-			if(mover_cursor_direita(cursor)){
-				cursor++;
-				cursor = line.erase(cursor);
-				cursor = line.erase(cursor);
-			}	
-			saida += letter;
-		}
-		if(letter == '<'){
-			if(mover_cursor_esquerda(cursor)){
-				cursor = line.erase(cursor);
-				cursor--;
+				cursor = saida.erase(cursor);
 			}
-			saida += letter;
 		}
-		if(letter == '>'){
-			if(mover_cursor_direita(cursor)){
-				cursor = line.erase(cursor);
-				cursor++;
+		if (letter == 'D') {
+			if (mover_cursor_direita(saida, cursor)) {
+				i++;
 			}
-			saida += letter;
 		}
+		// if (letter == '<') {
+		// 	if (mover_cursor_esquerda(saida, cursor)) {
+		// 		cursor = line.erase(cursor);
+		// 		cursor--;
+		// 	}
+		// 	saida += letter;
+		// }
+		// if (letter == '>') {
+		// 	if (mover_cursor_direita(saida, cursor)) {
+		// 		cursor = line.erase(cursor);
+		// 		cursor++;
+		// 	}
+		// 	saida += letter;
+		// }
 		cursor++;
 	}
+	saida += '|';
 	return saida;
 }
 
-void teste(string line){
-	auto it = line.begin();
-	it = it - 1;
-	cout << (int)*it << endl;
+void teste(string line) {
+	string t;
+	t += 'A';
+	t += 'M';
+	t += 'O';
+	t += 'R';
+	cout << t << endl;
 }
 
-int main(){
-	string line = "euRamo<<<";
-	//cin >> line;
-	//teste(line);
+int main() {
+	string line = "euRamDo";
+	// cin >> line;
+	// teste(line);
 	string saida = formatar(line);
-	//cadaCaracter(line);
+	// cadaCaracter(line);
 	cout << saida << endl;
 
 	return 0;
-} 
+}
