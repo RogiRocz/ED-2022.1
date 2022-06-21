3bbe97c9eb6bad4d84cb9fd465c5beda6e08dae6
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2022-06-21 19:08:29 +0000

    update Undo_Redo.cpp

--- S05 - List/Undo_Redo.cpp
+++ S05 - List/Undo_Redo.cpp
@@ -4,22 +4,16 @@
 
 using namespace std;
 
-int pos_cursor(string saida, string::iterator cursor) {
-	return distance(saida.begin(), cursor);
-}
-
-void mostrar(
-	list<pair<string, string::iterator>> undo,
-	list<pair<string, string::iterator>> redo) {
+void mostrar(list<pair<string, int>> undo, list<pair<string, int>> redo) {
 	for (auto elem : undo) {
-		cout << "saida: " << elem.first
-			 << " cursor: " << pos_cursor(elem.first, elem.second) << endl;
+		cout << "saida: " << elem.first << " cursor: " << elem.second << endl;
 	}
 
 	for (auto elem : redo) {
-		cout << "saida: " << elem.first
-			 << " cursor: " << pos_cursor(elem.first, elem.second) << endl;
+		cout << "saida: " << elem.first << " cursor: " << elem.second << endl;
 	}
+
+	cout << endl;
 }
 
 bool caracteresAceitos(char c) {
@@ -40,13 +34,15 @@ bool mover_cursor_direita(string saida, int pos_cursor) {
 string formatar(string line) {
 	string saida;
 	auto cursor = saida.begin();
-	list<pair<string, string::iterator>> undo;
-	auto cursorUndo = undo.begin();
-	list<pair<string, string::iterator>> redo;
-	auto cursorRedo = redo.begin();
+	list<pair<string, int>> undo;
+	int cursorUndo;
+	list<pair<string, int>> redo;
+	int cursorRedo;
+
+	auto pos_cursor = [&]() { return distance(saida.begin(), cursor); };
 
 	auto inserir_palavra = [&](string &saida, string::iterator cursor, char c) {
-		int j = pos_cursor(saida, cursor);
+		int j = pos_cursor();
 		string resto = saida.substr(j);
 		saida.resize(j + 1);
 		saida[j] = c;
@@ -56,56 +52,56 @@ string formatar(string line) {
 	for (int i = 0; i <= (int)line.size(); i++) {
 		auto letter = line[i];
 		if (caracteresAceitos(letter)) {
-			// cursorUndo = cursor;
-			undo.push_back(make_pair(saida, cursor));
+			cursorUndo = pos_cursor();
+			undo.push_back(make_pair(saida, cursorUndo));
 			inserir_palavra(saida, cursor, letter);
 			advance(cursor, +1);
 		}
 		if (letter == 'R') {
-			// cursorUndo = cursor;
-			undo.push_back(make_pair(saida, cursor));
+			cursorUndo = pos_cursor();
+			undo.push_back(make_pair(saida, cursorUndo));
 			letter = '\n';
 			inserir_palavra(saida, cursor, letter);
 			advance(cursor, +1);
 		}
 		if (letter == 'B') {
-			// cursorUndo = cursor;
-			undo.push_back(make_pair(saida, cursor));
-			if (mover_cursor_esquerda(saida, pos_cursor(saida, cursor))) {
+			cursorUndo = pos_cursor();
+			undo.push_back(make_pair(saida, cursorUndo));
+			if (mover_cursor_esquerda(saida, pos_cursor())) {
 				advance(cursor, -1);
 				cursor = saida.erase(cursor);
 			}
 		}
 		if (letter == 'D') {
-			if (mover_cursor_direita(saida, pos_cursor(saida, cursor))) {
-				// cursorUndo = cursor;
-				undo.push_back(make_pair(saida, cursor));
+			if (mover_cursor_direita(saida, pos_cursor())) {
+				cursorUndo = pos_cursor();
+				undo.push_back(make_pair(saida, cursorUndo));
 				cursor = saida.erase(cursor);
 			}
 		}
 		if (letter == '<') {
-			if (mover_cursor_esquerda(saida, pos_cursor(saida, cursor))) {
+			if (mover_cursor_esquerda(saida, pos_cursor())) {
 				advance(cursor, -1);
 			}
 		}
 		if (letter == '>') {
-			if (mover_cursor_direita(saida, pos_cursor(saida, cursor))) {
+			if (mover_cursor_direita(saida, pos_cursor())) {
 				advance(cursor, +1);
 			}
 		}
 		if (letter == 'Z') {
 			string saidaAux = saida;
-			auto cursorAux = cursor;
+			auto cursorAux = pos_cursor();
 			saida = undo.back().first;
-			cursor = undo.back().second;
+			advance(cursor, undo.back().second);
 			// cursorRedo = cursorAux;
 			redo.push_back(make_pair(saidaAux, cursorAux));
 		}
 		if (letter == 'Y') {
-			undo.back().first = saida;
-			undo.back().second = cursor;
-			cursor = redo.back().second;
 			saida = redo.back().first;
+			advance(cursor, redo.back().second);
+			undo.back().first = saida;
+			undo.back().second = pos_cursor();
 			redo.clear();
 		}
 		mostrar(undo, redo);
@@ -114,13 +110,7 @@ string formatar(string line) {
 	return saida;
 }
 
-list<string> teste(string line) {
-	list<string> t;
-	for (int i = 0; i < line.size(); i++) {
-		t.push_back(line);
-	}
-	return t;
-}
+list<string> teste(string line);
 
 void exibir(list<string> t) {
 	for (auto elem : t) {
@@ -132,7 +122,7 @@ int main() {
 	string line = "abcZZd";
 	// cin >> line;
 	cout << formatar(line) << endl;
-	// exibir(teste(line));
+	// teste(line);
 
 	return 0;
 }
