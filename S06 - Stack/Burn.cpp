621725467974ed7c806ca79878379fa629510eab
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2022-06-24 14:30:55 +0000

    update Burn.cpp create dadosBurn.txt

--- S06 - Stack/Burn.cpp
+++ S06 - Stack/Burn.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <stack>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
-int main(){
+struct Ponto{
+	int l, c;
+	//Ponto(int l, int c)l(l), c(c){}
+};
 
+int main(){
+	fstream file("dadosBurn.txt", fstream::in);
+	if(file.is_open()){
+		string line;
+		int l, c;
+		Ponto pontoInicial;
+		
+		getline(file, line);
+		line >> l >> c;
+		
+		
+		
+	}
+
+
+	file.close();
 	return 0;
 }
